23964adcb4b0a0700920a7bcb06142b43222407e
Nicolás Londoño <nicolas.londono@veevart.com>
2023-04-18 19:52:34 -0500

    Refactor: move logic

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,7 @@
-import React, { useEffect } from 'react'
+import React from 'react'
 import Posts from './components/Posts/Posts';
-import useAppContext from './customHooks/useAppContext';
-import getPosts from './utils/fetchPosts';
-import TYPES from './reducers/types';
 
 const App = () => {
-  const { dispatch } = useAppContext();
-
-  const handleGetPosts = async () => {
-    dispatch({ type: TYPES.FETCHING });
-    const posts = await getPosts()
-    dispatch({ type: TYPES.ADD_DATA, payload: posts });
-  }
-
-  useEffect(() => {
-    handleGetPosts()
-  }, []);
 
   return (
     <div>
--- src/components/Posts/Posts.jsx
+++ src/components/Posts/Posts.jsx
@@ -1,10 +1,22 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import useAppContext from '../../customHooks/useAppContext'
 import Post from '../Post/Post';
 import "./Posts.css";
+import TYPES from '../../reducers/types';
+import getPosts from '../../utils/fetchPosts';
 
 const Posts = () => {
-    const { state: { posts, isFetching } } = useAppContext();
+    const { state: { posts, isFetching }, dispatch } = useAppContext();
+
+    const handleGetPosts = async () => {
+        dispatch({ type: TYPES.FETCHING });
+        const posts = await getPosts()
+        dispatch({ type: TYPES.ADD_DATA, payload: posts });
+    }
+
+    useEffect(() => {
+        handleGetPosts()
+    }, []);
 
     return (
         <div className='posts'>
