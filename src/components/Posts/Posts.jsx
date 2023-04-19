dd54fc5ce012087077bf9ee1f85282dd68157bda
Nicolás Londoño <nicolas.londono@veevart.com>
2023-04-18 19:41:40 -0500

    Add: posts styles

--- src/components/Posts/Posts.jsx
+++ src/components/Posts/Posts.jsx
@@ -1,15 +1,16 @@
 import React from 'react'
 import useAppContext from '../../customHooks/useAppContext'
 import Post from '../Post/Post';
+import "./Posts.css";
 
 const Posts = () => {
     const { state: { posts, isFetching } } = useAppContext();
 
     return (
-        <div>
+        <div className='posts'>
             {isFetching ? <p>....cargando</p> : <ul>
                 {
-                    posts?.map(post => <Post key={post.id} {...post} />)
+                    posts?.map(post => <li key={post.id}><Post {...post} /></li>)
                 }
             </ul>}
         </div>
